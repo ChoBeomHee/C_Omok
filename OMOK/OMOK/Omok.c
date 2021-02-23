d90085a3c87b3ff0f76e4093773b2721e2bddd32
ChoBeomHee <68563167+ChoBeomHee@users.noreply.github.com>
2021-02-24 00:04:04 +0900

    Renzu update in progress

--- OMOK/OMOK/Omok.c
+++ OMOK/OMOK/Omok.c
@@ -126,46 +126,43 @@ int diagonalCheck2(char boad[MAX][MAX][MAX], int a, int b) {
 }
 // 대각선 검사 2
 
-
-
 int whithThreecheck(char boad[MAX][MAX][MAX], int a, int b) {
-
 	if ((strcmp(boad[a][b], boad[a + 1][b]) == 0 && strcmp(boad[a][b], boad[a + 2][b]) == 0) ||						// 세로 검사
 		(strcmp(boad[a][b], boad[a - 1][b]) == 0 && strcmp(boad[a][b], boad[a + 1][b]) == 0) ||
 		(strcmp(boad[a][b], boad[a - 1][b]) == 0 && strcmp(boad[a][b], boad[a - 2][b]) == 0))
 		return 1;
 
-	if ((strcmp(boad[a][b], boad[a + 3][b]) == 0 && (strcmp(boad[a][b], boad[a + 2][b]) == 0 || strcmp(boad[a][b], boad[a + 1][b]) == 0)) ||
+	else if ((strcmp(boad[a][b], boad[a + 3][b]) == 0 && (strcmp(boad[a][b], boad[a + 2][b]) == 0 || strcmp(boad[a][b], boad[a + 1][b]) == 0)) ||
 		(strcmp(boad[a][b], boad[a - 3][b]) == 0 && (strcmp(boad[a][b], boad[a - 2][b]) == 0 || strcmp(boad[a][b], boad[a - 1][b]) == 0)))// 세로 검사 2
-		return 1;																									
+		return 1;
 
-	if ((strcmp(boad[a][b], boad[a - 1][b]) == 0 && (strcmp(boad[a][b], boad[a + 2][b]) == 0)) ||
+	else if ((strcmp(boad[a][b], boad[a - 1][b]) == 0 && (strcmp(boad[a][b], boad[a + 2][b]) == 0)) ||
 		(strcmp(boad[a][b], boad[a + 1][b]) == 0 && (strcmp(boad[a][b], boad[a - 2][b]) == 0)))						// 세로 검사 3
 		return 1;
 
-	if ((strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a - 2][b + 2]) == 0) ||				// 대각선 1 검사
+	else if ((strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a - 2][b + 2]) == 0) ||				// 대각선 1 검사
 		(strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a + 1][b - 1]) == 0) ||
 		(strcmp(boad[a][b], boad[a + 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a + 2][b - 2]) == 0))
 		return 1;
-	
-	if ((strcmp(boad[a][b], boad[a - 3][b + 3]) == 0 && (strcmp(boad[a][b], boad[a - 2][b + 2]) == 0 || strcmp(boad[a][b], boad[a - 1][b + 1]) == 0)) ||
-		(strcmp(boad[a][b], boad[a + 3][b - 3]) == 0 && (strcmp(boad[a][b], boad[a + 2][b - 2]) == 0 || strcmp(boad[a][b], boad[a + 1][b - 1]) == 0)))		// 대각선1 검사 2
-		return 1;	
 
-	if ((strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && (strcmp(boad[a][b], boad[a + 2][b - 2]) == 0)) ||
+	else if ((strcmp(boad[a][b], boad[a - 3][b + 3]) == 0 && (strcmp(boad[a][b], boad[a - 2][b + 2]) == 0 || strcmp(boad[a][b], boad[a - 1][b + 1]) == 0)) ||
+		(strcmp(boad[a][b], boad[a + 3][b - 3]) == 0 && (strcmp(boad[a][b], boad[a + 2][b - 2]) == 0 || strcmp(boad[a][b], boad[a + 1][b - 1]) == 0)))		// 대각선1 검사 2
+		return 1;
+
+	else if ((strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && (strcmp(boad[a][b], boad[a + 2][b - 2]) == 0)) ||
 		(strcmp(boad[a][b], boad[a + 1][b - 1]) == 0 && (strcmp(boad[a][b], boad[a - 2][b + 2]) == 0)))						// 대각선1 검사 3
 		return 1;
 
-	if ((strcmp(boad[a][b], boad[a - 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a - 2][b - 2]) == 0) ||						// 대각선 2 검사
+	else if ((strcmp(boad[a][b], boad[a - 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a - 2][b - 2]) == 0) ||						// 대각선 2 검사
 		(strcmp(boad[a][b], boad[a - 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a + 1][b + 1]) == 0) ||
 		(strcmp(boad[a][b], boad[a + 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a + 2][b + 2]) == 0))
 		return 1;
 
-	if ((strcmp(boad[a][b], boad[a - 3][b - 3]) == 0 && (strcmp(boad[a][b], boad[a - 2][b - 2]) == 0 || strcmp(boad[a][b], boad[a - 1][b - 1]) == 0)) ||
+	else if ((strcmp(boad[a][b], boad[a - 3][b - 3]) == 0 && (strcmp(boad[a][b], boad[a - 2][b - 2]) == 0 || strcmp(boad[a][b], boad[a - 1][b - 1]) == 0)) ||
 		(strcmp(boad[a][b], boad[a + 3][b + 3]) == 0 && (strcmp(boad[a][b], boad[a + 2][b + 2]) == 0 || strcmp(boad[a][b], boad[a + 1][b + 1]) == 0)))		// 대각선2 검사 2
 		return 1;
 
-	if ((strcmp(boad[a][b], boad[a + 1][b + 1]) == 0 && (strcmp(boad[a][b], boad[a - 2][b - 2]) == 0)) ||
+	else if ((strcmp(boad[a][b], boad[a + 1][b + 1]) == 0 && (strcmp(boad[a][b], boad[a - 2][b - 2]) == 0)) ||
 		(strcmp(boad[a][b], boad[a - 1][b - 1]) == 0 && (strcmp(boad[a][b], boad[a + 2][b + 2]) == 0)))						// 대각선2 검사 3
 		return 1;
 }
@@ -293,25 +290,308 @@ int diagonalThreecheck2(char boad[MAX][MAX][MAX], int a, int b) {
 		return 1;
 }
 // 33대각선2 검사
-//	33검사 Case 1
-
-
-
+// 33검사 Case 1
 
 int whithCheckCase2(char boad[MAX][MAX][MAX], int a, int b) {
+	int result = 0;
+	if (((strcmp(boad[a][b], boad[a][b + 1]) == 0 && strcmp(boad[a][b], boad[a][b + 3]) == 0) && strcmp(boad[a][b], "○") == 0)) {	// 가로 case 1
+		for (int i = 0; i < 4; i++)
+		{
+			if (i == 2)
+				continue;
 
+			else if (whithThreecheck(boad, a, b + i) == 1)
+				result = whithThreecheck(boad, a, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a][b - 1]) == 0 && strcmp(boad[a][b], boad[a][b + 2]) == 0) && strcmp(boad[a][b], "○") == 0) {	// 가로 case 2
+		for (int i = -1; i < 2; i++)
+		{
+			if (i == 1)
+				continue;
+			if (whithThreecheck(boad, a, b + i) == 1)
+				result = whithThreecheck(boad, a, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a][b - 2]) == 0 && strcmp(boad[a][b], boad[a][b - 3]) == 0) && strcmp(boad[a][b], "○") == 0) { // 가로 case 3
+		for (int i = -3; i < 1; i++)
+		{
+			if (i == -1)
+				continue;
+			if (whithThreecheck(boad, a, b + i) == 1)
+				result = whithThreecheck(boad, a, b + i);
+		}
+		return result;
+	}
+	
+	else if ((strcmp(boad[a][b], boad[a][b + 2]) == 0 && strcmp(boad[a][b], boad[a][b + 3]) == 0) && strcmp(boad[a][b], "○") == 0) { // 가로 case 4
+		for (int i = 0; i < 4; i++)
+		{
+			if (i == 1)
+				continue;
+			if (whithThreecheck(boad, a, b + i) == 1)
+				result = whithThreecheck(boad, a, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a][b + 1]) == 0 && strcmp(boad[a][b], boad[a][b - 2]) == 0) && strcmp(boad[a][b], "○") == 0) { // 가로 case 5
+		for (int i = -2; i < 2; i++)
+		{
+			if (i == -1)
+				continue;
+
+			if (whithThreecheck(boad, a, b + i) == 1)
+				result = whithThreecheck(boad, a, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a][b - 1]) == 0 && strcmp(boad[a][b], boad[a][b - 3]) == 0) && strcmp(boad[a][b], "○") == 0) { // 가로 case 6
+		for (int i = -3; i < 1; i++) {
+			if (i == -2)
+				continue;
+			if (whithThreecheck(boad, a, b + i) == 1)
+				result = whithThreecheck(boad, a, b + i);
+		}
+		return result;
+	}
 }
 int HeightCheckCase2(char boad[MAX][MAX][MAX], int a, int b) {
+	int result = 0;
+	if (((strcmp(boad[a][b], boad[a + 1][b]) == 0 && strcmp(boad[a][b], boad[a + 3][b]) == 0) && strcmp(boad[a][b], "○") == 0)) {	//case 1
+		for (int i = 0; i < 4; i++)
+		{
+			if (i == 2)
+				continue;
 
+			else if (HeightThreecheck(boad, a + i, b) == 1)
+				result = HeightThreecheck(boad, a + i, b);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a - 1][b]) == 0 && strcmp(boad[a][b], boad[a + 2][b]) == 0) && strcmp(boad[a][b], "○") == 0) {	//case 2
+		for (int i = -1; i < 3; i++)
+		{
+			if (i == 1)
+				continue;
+			if (HeightThreecheck(boad, a + i, b ) == 1)
+				result = HeightThreecheck(boad, a + i, b);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a - 2][b]) == 0 && strcmp(boad[a][b], boad[a - 3][b]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 3
+		for (int i = -3; i < 1; i++)
+		{
+			if (i == -1)
+				continue;
+			if (HeightThreecheck(boad, a + i, b) == 1)
+				result = HeightThreecheck(boad, a + i, b);
+		}
+		return result;
+	}
+
+	else if ((strcmp(boad[a][b], boad[a + 2][b]) == 0 && strcmp(boad[a][b], boad[a + 3][b]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 4
+		for (int i = 0; i < 4; i++)
+		{
+			if (i == 1)
+				continue;
+			if (HeightThreecheck(boad, a + i, b) == 1)
+				result = HeightThreecheck(boad, a + i, b);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a + 1][b]) == 0 && strcmp(boad[a][b], boad[a - 2][b]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 5
+		for (int i = -2; i < 2; i++)
+		{
+			if (i == -1)
+				continue;
+
+			if (HeightThreecheck(boad, a + i, b) == 1)
+				result = HeightThreecheck(boad, a + i, b);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a - 1][b]) == 0 && strcmp(boad[a][b], boad[a - 3][b]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 6
+		for (int i = -3; i < 1; i++) {
+			if (i == -2)
+				continue;
+			if (HeightThreecheck(boad, a + i, b) == 1)
+				result = HeightThreecheck(boad, a + i, b);
+		}
+		return result;
+	}
 }
 int diagonalCheckCase12(char boad[MAX][MAX][MAX], int a, int b) {
+	int result = 0;
+	if (((strcmp(boad[a][b], boad[a - 2][b + 2]) == 0 && strcmp(boad[a][b], boad[a - 3][b + 3]) == 0) && strcmp(boad[a][b], "○") == 0)) {	// case 1
+		for (int i = 0; i < 4; i++){
+			if (i == 1)
+				continue;
 
+			else if (diagonalThreecheck1(boad, a - i, b + i) == 1)
+				result = diagonalThreecheck1(boad, a - i, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a + 2][b - 2]) == 0 && strcmp(boad[a][b], boad[a - 1][b + 1]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 2
+		for (int i = -2; i < 2; i++)
+		{
+			if (i == -1)
+				continue;
+			if (diagonalThreecheck1(boad, a - i, b + i) == 1)
+				result = diagonalThreecheck1(boad, a - i, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a + 3][b - 3]) == 0 && strcmp(boad[a][b], boad[a + 1][b - 1]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 3
+		for (int i = -3; i < 1; i++)
+		{
+			if (i == -2)
+				continue;
+			if (diagonalThreecheck1(boad, a - i, b + i) == 1)
+				result = diagonalThreecheck1(boad, a - i, b + i);
+		}
+		return result;
+	}
+
+	else if ((strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a - 3][b + 3]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 4
+		for (int i = 0; i < 4; i++)
+		{
+			if (i == 2)
+				continue;
+			if (diagonalThreecheck1(boad, a - i, b + i) == 1)
+				result = diagonalThreecheck1(boad, a - i, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a + 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a - 2][b + 2]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 5
+		for (int i = -1; i < 3; i++)
+		{
+			if (i == 1)
+				continue;
+
+			if (diagonalThreecheck1(boad, a - i, b + i) == 1)
+				result = diagonalThreecheck1(boad, a - i, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a - 3][b + 3]) == 0 && strcmp(boad[a][b], boad[a - 2][b + 2]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 6
+		for (int i = -3; i < 1; i++) {
+			if (i == -1)
+				continue;
+			if (diagonalThreecheck1(boad, a - i, b + i) == 1)
+				result = diagonalThreecheck1(boad, a - i, b + i);
+		}
+		return result;
+	}
 }
 int diagonalCheckCase22(char boad[MAX][MAX][MAX], int a, int b) {
+	int result = 0;
+	if (((strcmp(boad[a][b], boad[a + 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a + 3][b + 3]) == 0) && strcmp(boad[a][b], "○") == 0)) {	// case 1
+		for (int i = 0; i < 4; i++) {
+			if (i == 2)
+				continue;
 
+			else if (diagonalThreecheck2(boad, a + i, b + i) == 1)
+				result = diagonalThreecheck2(boad, a + i, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a - 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a + 2][b + 2]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 2
+		for (int i = -1; i < 3; i++)
+		{
+			if (i == 1)
+				continue;
+			if (diagonalThreecheck2(boad, a + i, b + i) == 1)
+				result = diagonalThreecheck2(boad, a + i, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a - 2][b - 2]) == 0 && strcmp(boad[a][b], boad[a - 3][b - 3]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 3
+		for (int i = -3; i < 1; i++)
+		{
+			if (i == -1)
+				continue;
+			if (diagonalThreecheck2(boad, a + i, b + i) == 1)
+				result = diagonalThreecheck2(boad, a + i, b + i);
+		}
+		return result;
+	}
+
+	else if ((strcmp(boad[a][b], boad[a + 2][b + 2]) == 0 && strcmp(boad[a][b], boad[a + 3][b + 3]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 4
+		for (int i = 0; i < 4; i++)
+		{
+			if (i == 1)
+				continue;
+			if (diagonalThreecheck2(boad, a + i, b + i) == 1)
+				result = diagonalThreecheck2(boad, a + i, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a + 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a - 2][b - 2]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 5
+		for (int i = -2; i < 2; i++)
+		{
+			if (i == -1)
+				continue;
+
+			if (diagonalThreecheck2(boad, a + i, b + i) == 1)
+				result = diagonalThreecheck2(boad, a + i, b + i);
+		}
+		return result;
+	}
+	else if ((strcmp(boad[a][b], boad[a - 3][b - 3]) == 0 && strcmp(boad[a][b], boad[a - 1][b - 1]) == 0) && strcmp(boad[a][b], "○") == 0) { // case 6
+		for (int i = -3; i < 1; i++) {
+			if (i == -2)
+				continue;
+			if (diagonalThreecheck2(boad, a + i, b + i) == 1)
+				result = diagonalThreecheck2(boad, a + i, b + i);
+		}
+		return result;
+	}
 }
-
+/*
 int whithThreeCheckCase2(char boad[MAX][MAX], int a, int b) {
+
+
+		if ((strcmp(boad[a][b], boad[a + 1][b]) == 0 && strcmp(boad[a][b], boad[a + 2][b]) == 0) ||						// 세로 검사
+			(strcmp(boad[a][b], boad[a - 1][b]) == 0 && strcmp(boad[a][b], boad[a + 1][b]) == 0) ||
+			(strcmp(boad[a][b], boad[a - 1][b]) == 0 && strcmp(boad[a][b], boad[a - 2][b]) == 0))
+			return 1;
+
+		if ((strcmp(boad[a][b], boad[a + 3][b]) == 0 && (strcmp(boad[a][b], boad[a + 2][b]) == 0 || strcmp(boad[a][b], boad[a + 1][b]) == 0)) ||
+			(strcmp(boad[a][b], boad[a - 3][b]) == 0 && (strcmp(boad[a][b], boad[a - 2][b]) == 0 || strcmp(boad[a][b], boad[a - 1][b]) == 0)))// 세로 검사 2
+			return 1;
+
+		if ((strcmp(boad[a][b], boad[a - 1][b]) == 0 && (strcmp(boad[a][b], boad[a + 2][b]) == 0)) ||
+			(strcmp(boad[a][b], boad[a + 1][b]) == 0 && (strcmp(boad[a][b], boad[a - 2][b]) == 0)))						// 세로 검사 3
+			return 1;
+
+		if ((strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a - 2][b + 2]) == 0) ||				// 대각선 1 검사
+			(strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a + 1][b - 1]) == 0) ||
+			(strcmp(boad[a][b], boad[a + 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a + 2][b - 2]) == 0))
+			return 1;
+
+		if ((strcmp(boad[a][b], boad[a - 3][b + 3]) == 0 && (strcmp(boad[a][b], boad[a - 2][b + 2]) == 0 || strcmp(boad[a][b], boad[a - 1][b + 1]) == 0)) ||
+			(strcmp(boad[a][b], boad[a + 3][b - 3]) == 0 && (strcmp(boad[a][b], boad[a + 2][b - 2]) == 0 || strcmp(boad[a][b], boad[a + 1][b - 1]) == 0)))		// 대각선1 검사 2
+			return 1;
+
+		if ((strcmp(boad[a][b], boad[a - 1][b + 1]) == 0 && (strcmp(boad[a][b], boad[a + 2][b - 2]) == 0)) ||
+			(strcmp(boad[a][b], boad[a + 1][b - 1]) == 0 && (strcmp(boad[a][b], boad[a - 2][b + 2]) == 0)))						// 대각선1 검사 3
+			return 1;
+
+		if ((strcmp(boad[a][b], boad[a - 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a - 2][b - 2]) == 0) ||						// 대각선 2 검사
+			(strcmp(boad[a][b], boad[a - 1][b - 1]) == 0 && strcmp(boad[a][b], boad[a + 1][b + 1]) == 0) ||
+			(strcmp(boad[a][b], boad[a + 1][b + 1]) == 0 && strcmp(boad[a][b], boad[a + 2][b + 2]) == 0))
+			return 1;
+
+		if ((strcmp(boad[a][b], boad[a - 3][b - 3]) == 0 && (strcmp(boad[a][b], boad[a - 2][b - 2]) == 0 || strcmp(boad[a][b], boad[a - 1][b - 1]) == 0)) ||
+			(strcmp(boad[a][b], boad[a + 3][b + 3]) == 0 && (strcmp(boad[a][b], boad[a + 2][b + 2]) == 0 || strcmp(boad[a][b], boad[a + 1][b + 1]) == 0)))		// 대각선2 검사 2
+			return 1;
+
+		if ((strcmp(boad[a][b], boad[a + 1][b + 1]) == 0 && (strcmp(boad[a][b], boad[a - 2][b - 2]) == 0)) ||
+			(strcmp(boad[a][b], boad[a - 1][b - 1]) == 0 && (strcmp(boad[a][b], boad[a + 2][b + 2]) == 0)))						// 대각선2 검사 3
+			return 1;
 
 }
 int HeightThreeCheckCase2(char boad[MAX][MAX], int a, int b) {
@@ -322,7 +602,7 @@ int diagonal1_ThreeCheckCase2(char boad[MAX][MAX], int a, int b) {
 }
 int diagonal2_ThreeCheckCase2(char boad[MAX][MAX], int a, int b) {
 
-}
+}*/
 void Printboad(char boad[MAX][MAX][MAX]) {
 	gotoxy(0, 0);
 	for (int i = 0; i < MAX; i++) {
@@ -343,7 +623,8 @@ void Playchoice1(char boad[MAX][MAX][MAX], int a, int b) {
 		a = (a / 2);
 
 	strcpy(boad[b][a], "○");
-	if (whithCheck(boad, b, a) == 1 || HeightCheck(boad, b, a) == 1 || diagonalCheck1(boad, b, a) == 1 || diagonalCheck2(boad, b, a) == 1)
+	if (whithCheckCase2(boad, b, a) == 1 ||  HeightCheckCase2(boad, b, a) == 1 || diagonalCheckCase12(boad, b, a) == 1 || diagonalCheckCase22(boad, b, a) == 1 ||
+		whithCheck(boad, b, a) == 1 || HeightCheck(boad, b, a) == 1 || diagonalCheck1(boad, b, a) == 1 || diagonalCheck2(boad, b, a) == 1)
 		printf("		테스트 성공%d", n);
 	Printboad(boad);
 	if (Matchplay1(boad) == 1)
